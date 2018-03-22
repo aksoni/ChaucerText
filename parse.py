e55770a27b752a8c5b555830a1fcfc7960a580a2
Abhishek Soni <abhisheksoni@Abhisheks-Air.wi.rr.com>
2018-03-22 14:08:47 -0500

    First commit

---
+++ parse.py
@@ -0,0 +1,18 @@
+import sys
+
+with open("tales/gp-par.htm") as file:
+    i = 1
+    readingText = False
+    for n, line in enumerate(file):
+
+        if line[0].isdigit() and i == int(line[0]):
+            readingText = True
+        
+        if readingText == True and line[0].isdigit():
+    	    print("first line")
+    	    print(line)
+        # i += 1
+        elif readingText == True and line[0] is '&':
+            print("second line")
+            print(line)
+
