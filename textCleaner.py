e34b3fdf7bc72250214df4c73a6ac27930b9dc4b
Abhishek Soni <abhisheksoni@Abhisheks-Air.wi.rr.com>
2018-03-22 17:41:16 -0500

    Working on parsing through all tales

--- textCleaner.py
+++ textCleaner.py
@@ -25,11 +25,15 @@ for textFile in filesList:
     soup = BeautifulSoup(open(textFile), "html.parser")
     cleanFile = textFile.split('_')[0]+"_original_clean.txt"
     fOut = open(cleanFile, 'w')
+    #print(textFile)
+    #if textFile == "talesText/clkenv-par_original_messy.txt":
+    #   print("b string")
     for b in soup.findAll('b'):
+        #b.getText()
         if b.string is not None:
-            fOut.write(b.string)
+            fOut.write(b.getText())
             fOut.write('\n')
-            fOut2.write(b.string)
+            fOut2.write(b.getText())
             fOut2.write('\n')
 
     #print(i)
