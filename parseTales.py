c20820e2c1daced8c902a67f27c5bfbc35d49ae5
Abhishek Soni <abhisheksoni@Abhisheks-Air.wi.rr.com>
2018-03-22 23:29:37 -0500

    Fixing errors in parsing

--- parseTales.py
+++ parseTales.py
@@ -51,7 +51,7 @@ for htmlFile in filesList:
                                                                                          line.lstrip()[0] == 'n' or line.lstrip()[0:4] == '</B>'))):#line[0] is '&' or line[0] is ' &' or (len(line.lstrip()) > 0 and line.lstrip()[0] == '&'):
                 for word in delete_list:
                     line = line.replace(word, "")
-                if line.lstrip()[0:4] != "[The" and line.lstrip()[0:3] != "<i>":
+                if line.lstrip()[0:4] != "[The" and line.lstrip()[0:3] != "<i>" and line.lstrip()[0:3] != "[Co":
                     fOut2.write(line.lstrip())
                     fOut4.write(line.lstrip())
                 #print("second line")
