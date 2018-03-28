aec40ce9bc7e7720de320ac63f7bb0163e69e6f0
Abhishek Soni <abhisheksoni@Abhisheks-MacBook-Air.local>
2018-03-28 00:34:35 -0400

    Split text data into training, validation, and testing files

--- dict_formatter.py
+++ dict_formatter.py
@@ -7,57 +7,25 @@ if not os.path.exists('data'):
 fOut = open('data/chaucer.dict', 'w')
 
 with open("chaucer_glossary.txt") as file:
-    #wordlist = [line.split(None, 1)[0] for line in file]
-#print(wordlist)
 
     for i, line in enumerate(file):
-        # print(originalList)
+        
         print(line)
+        
         str = line.split('\t')
+        
         original = str[0]
+        
         modern_messy = str[1]
         modern_messy = modern_messy.split(',')[0].rstrip()
         modern_clean = modern_messy.split(';')[0].rstrip()
-            # if ',' in modern_messy:
-            #modern_clean = modern_messy.split(',')[0].rstrip()
-            #elif ';' in modern_messy:
-            #modern_clean = modern_messy.split(';')[0].rstrip()
-            #else:
-            #modern_clean = modern_messy.rstrip()
-            # print(modern_clean)
-            #print(re.sub(r'\(.*\)', '', modern_clean))
+     
         for word in original.split(','):
-            #word = word.replace(",", "")
+            
             word = word.lstrip()
             line = word + '\t' + modern_clean
-            #print(line)
+    
             fOut.write(line)
             fOut.write('\n')
-        #modern_clean = modern_messy.split()
-        # print(original)
-        #  print(len(original.split()))
-        # print(modern_clean)
-            #for word in original.split():
-            #originalList.append(word)
-        #print(original)
-        #print(len(original.split()))
-        #str = "".join(str[1])
-        #print(str[1])
-        #print(str)
-        #print(len(str.split()))
-        #str = "".join(str[1].split(';'))
-        #if len(str) > 1:
-            #str = str[0]
-            #else:
-            #str = str.split(',')
-            # print(str)
-            #   print(len(str))
-        
-        #print(str[1])
-        #if i > 30:
-#   break
+
 fOut.close()
-#  break
-
-# print(originalList)
-
--- get_all_texts.py
+++ get_all_texts.py
@@ -23,9 +23,11 @@ pywalker(top)
 if not os.path.exists('allText'):
     os.makedirs('allText')
 
+#Creates data directory if it does not exist
 if not os.path.exists('data'):
     os.makedirs('data')
 
+#Open files in which to write lines
 fOut = open('allText/allModernText.txt', 'w')
 fOut2 = open('data/train.modern.nltkok', 'w')
 fOut3 = open('data/train.original.nltkok', 'w')
@@ -34,16 +36,25 @@ fOut5 = open('data/test.modern.nltkok', 'w')
 fOut6 = open('data/valid.original.nltkok', 'w')
 fOut7 = open('data/test.original.nltkok', 'w')
 
-#Write all modern lines to file
+
+
+
 for textFile in modernFilesList:
     with open(textFile) as file:
         for line in file:
+            #Write all modern lines to allText file
             fOut.write(line)
+            
             if textFile == "talesText/wbt-par_modern_clean.txt":
+                #Write Wife of Bath's tale to validation file
                 fOut4.write(line)
+            
             elif textFile == "talesText/mlt-par_modern_clean.txt":
+                #Write Man of Law's tale to test file
                 fOut5.write(line)
+            
             else:
+                #Write all other tales to training file
                 fOut2.write(line)
 fOut.close()
 fOut2.close()
@@ -52,16 +63,23 @@ fOut5.close()
 
 fOut8 = open('allText/allOriginalText.txt', 'w')
 
-#Write all original lines to file
+
 for textFile in originalFilesList:
     with open(textFile) as file:
         for line in file:
+            #Write all original lines to file
             fOut8.write(line)
+            
             if textFile == "talesText/wbt-par_original_clean.txt":
+                #Write Wife of Bath's tale to validation file
                 fOut6.write(line)
+            
             elif textFile == "talesText/mlt-par_original_clean.txt":
+                #Write Man of Law's tale to test file
                 fOut7.write(line)
+            
             else:
+                #Write all other tales to training file
                 fOut3.write(line)
 
 fOut3.close()
